fc53168ebc523cb97a98378e687887dcdee2bf5b
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-09-25 22:06:21 -0400

    create branch for localhost

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,11 +1,11 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-      return $http.get('https://floating-hamlet-84385.herokuapp.com/api/listings');
+      return $http.get('http://localhost:8080/api/listings');
     },
 	
 	create: function(listing) {
-	  return $http.post('https://floating-hamlet-84385.herokuapp.com/api/listings', listing);
+	  return $http.post('http://localhost:8080/api/listings', listing);
     }, 
 
     delete: function(listingId) {
